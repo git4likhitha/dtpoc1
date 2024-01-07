5c89623631461241b62a5ba938369d521e982f75
daisy.ptrm@gmail.com <daisy.ptrm@gmail.com>
2024-01-07 07:04:20 +0000

    changes made in command

--- tests/createtesttbl.sql
+++ tests/createtesttbl.sql
@@ -1,1 +1,2 @@
-create table as select * from `genuine-rope-410306.dbtpoc.tbl_dbt_poc` limit 1000
+create table `genuine-rope-410306.dbtpoc.tbl_trnsfm_dbt_poc` as 
+select * from `genuine-rope-410306.dbtpoc.tbl_dbt_poc` limit 1000
